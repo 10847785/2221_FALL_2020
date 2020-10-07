a94c1235c15f7e6ae85530b65d9bc4ea8c1b2956
Ericio <eric322000@gmail.com>
2020-10-07 17:06:12 -0600

    Walk, Run, Jump, Double Jump
    Added charcter movements

---
+++ Jump/Assets/Scenes/Scripts/PlayerJump.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerJump : MonoBehaviour
+{
+    private CharacterController controller;
+   private Vector3 movement;
+
+   public float gravity = -9.81f, jumpForce = 3f;
+   private float yVar;
+
+   public IntData playerJumpCount;
+   private int jumpCount;
+
+   private void Start()
+   {
+      controller = GetComponent<CharacterController>();
+   }
+
+   private void Update()
+   {
+       if (controller.isGrounded && movement.y < 0)
+       {
+           yVar = -1f;
+           jumpCount = 0;
+       }
+
+       if (Input.GetButtonDown("Jump") && jumpCount < playerJumpCount.value)
+       {
+           yVar = jumpForce;
+           jumpCount++;
+       }
+
+       movement.y = yVar;
+       controller.Move(movement * Time.deltaTime);
+       yVar += gravity * Time.deltaTime;
+   }
+}
---
+++ Walk_Run/Walk_Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private FloatData moveSpeed;
+    public FloatData normalSpeed, fastSpeed;
+    public float rotateSpeed = 30f, gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 movement;
+    private float yVar;
+    private bool canMove = true;
+
+    private void Start()
+    {
+        moveSpeed = normalSpeed;
+        controller = GetComponent<CharacterController>();
+        StartCoroutine(Move());
+    }
+    
+    private readonly WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private IEnumerator Move()
+    {
+        canMove = true;
+        while (canMove)
+        {
+            yield return wffu;
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                moveSpeed = fastSpeed;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                moveSpeed = normalSpeed;
+            }
+
+            if (controller.isGrounded && movement.y < 0)
+            {
+                yVar = -1f;
+            }
+        
+            var hInput = Input.GetAxis("Horizontal") * Time.deltaTime*rotateSpeed;
+            transform.Rotate(0, hInput, 0);
+            var vInput = Input.GetAxis("Vertical") * moveSpeed.value;
+            movement.Set(vInput, yVar, 0);
+
+            movement = transform.TransformDirection(movement);
+            controller.Move(movement * Time.deltaTime);
+            yVar += gravity * Time.deltaTime;
+        }
+        
+    }
+}
