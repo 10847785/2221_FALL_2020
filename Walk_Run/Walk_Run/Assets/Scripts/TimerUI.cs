8797ef3d82ffe9e6e01b96a8077d4b348fae2660
Ericio <eric322000@gmail.com>
2020-10-12 16:53:04 -0600

    TriggerEvents
    Worked with Triggers

---
+++ Walk_Run/Walk_Run/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public int maxTime;
+    public IntData time;
+    public Text timerText;
+
+    void Start()
+    {
+        time.value = maxTime;
+    }
+
+    public IEnumerator Countdown()
+    {
+        time.value = maxTime;
+        while (time.value >= 0)
+        {
+            DisplayTimer();
+            yield return new WaitForSeconds(1f);
+            time.value--;
+        }
+    }
+
+    public void DisplayTimer()
+    {
+        timerText.text = time.value.ToString();
+    }
+}
---
+++ Walk_Run/Walk_Run/Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class TriggerBehaviour : MonoBehaviour
+{
+    public Color newColor;
+    public Color defaultColor;
+    private MeshRenderer colorChange;
+
+    public bool isOpen;
+    private WaitForSeconds wfs;
+    public int holdTime = 3;
+    public GameObject door;
+    public TimerUI timer;
+    private void Start()
+    {
+        colorChange = GetComponent<MeshRenderer>();
+        colorChange.material.color = defaultColor;
+        
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        newColor.a = 0.5f;
+        colorChange.material.color = newColor;
+        
+        door.SetActive(false);
+        
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        colorChange.material.color = defaultColor;
+       
+        StartCoroutine(timer.Countdown());
+        yield return wfs;
+        isOpen = false;
+        door.SetActive(true);
+    }
+    
+}
