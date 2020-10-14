64fa3371b31622ba1175058f3d1d1ee632ccf479
Ericio <eric322000@gmail.com>
2020-10-14 15:37:15 -0600

    Bullet

---
+++ Walk_Run/Walk_Run/Assets/Scripts/Shooting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Shooting : MonoBehaviour
+{
+    public int ammoCount = 100;
+    public GameObject prefab;
+    public float bulletForce;
+    public Transform Instancer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("Fire1") && ammoCount > 0)
+        {
+            Instantiate(prefab, Instancer.position, Instancer.rotation);
+        }
+    }
+}
