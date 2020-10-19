98bad79c236592267a16fbc3a6103944e09d9557
Ericio <eric322000@gmail.com>
2020-10-19 17:03:36 -0600

    Damage and Spawning

---
+++ Walk_Run/Walk_Run/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float damageAmount = 20f;
+    public FloatData value;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("The damage is " + damageAmount);
+            value.value -= damageAmount;
+        }
+    }
+}
---
+++ Walk_Run/Walk_Run/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    public FloatData value;
+    public GameObject spawnPoint;
+    
+
+    private void Update()
+    {
+        if (value.value <=0f)
+        {
+            gameObject.transform.position = spawnPoint.transform.position;
+        }
+    }
+}
