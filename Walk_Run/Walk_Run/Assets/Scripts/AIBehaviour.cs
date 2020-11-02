afce79c5a37302e4a1a13dd93ae35274d30cdc72
Ericio <eric322000@gmail.com>
2020-11-02 16:08:17 -0700

    More Touch Ups

---
+++ Walk_Run/Walk_Run/Assets/Scripts/AIBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    private WaitForFixedUpdate wffu = new WaitForFixedUpdate();
+    private NavMeshAgent agent;
+    public Transform player;
+    private bool canHunt, canNav, canPatrol = true;
+    public List<Transform> patrolPoints;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        canHunt = true;
+        canPatrol = false;
+        agent.destination = player.position;
+        var distance = agent.remainingDistance;
+        while (distance <= 0.25f)
+        {
+            distance = agent.remainingDistance;
+            yield return wffu;
+        }
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(canHunt ? OnTriggerEnter(other) : Patrol());
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canNav = false;
+    }
+
+    private IEnumerator Patrol()
+    {
+        canPatrol = true;
+        while (canPatrol)
+        {
+            yield return wffu;
+            if (agent.pathPending || !(agent.remainingDistance < 0.5f)) continue;
+          //  agent.destination = patrolPoints[i].position;
+          //  i = (i + 1) % patrolPoints.Count;
+        }
+    }
+}
