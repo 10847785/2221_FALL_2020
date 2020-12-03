811652a533b3355fccc0a757eab17dee5e3e88d0
Ericio <eric322000@gmail.com>
2020-12-02 17:16:51 -0700

    Enemy is near completion

---
+++ Walk_Run/Walk_Run/Assets/Scripts/NestDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestDamage : MonoBehaviour
+{
+    public float damageAmount = 20f;
+    public FloatData nestHealth, nestMax;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Nest it hit " + damageAmount);
+            nestHealth.value += damageAmount;
+            nestHealth.value = Mathf.Clamp(nestHealth.value, 0f, nestMax.value);
+        }
+    }
+}
