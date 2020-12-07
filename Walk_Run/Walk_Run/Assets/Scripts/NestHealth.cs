22059dae16afae654dd50e5c3ddb59a5beb68339
Ericio <eric322000@gmail.com>
2020-12-07 16:12:48 -0700

    Enemy Rework 1

---
+++ Walk_Run/Walk_Run/Assets/Scripts/NestHealth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NestHealth : MonoBehaviour
+{
+    //FloatData nestHealth
+    //FloatData maxHealth
+    //if statement if health is at 0 destroy certain gameObjects
+    
+}
---
+++ Walk_Run/Walk_Run/Assets/Scripts/NestTakeDamage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestTakeDamage : MonoBehaviour
+{
+    //FloatData health
+    //UnityEvent to NestHealth
+    //OnTriggerEnter
+    //if (other.gameObject.layer == 10)
+    //    health.data -= amount(either float or FloatData);    
+    
+    
+    
+}
