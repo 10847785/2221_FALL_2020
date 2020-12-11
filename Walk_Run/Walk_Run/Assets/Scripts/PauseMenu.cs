660113654608e92be0bc4b25113d0e79fb60c980
Ericio <eric322000@gmail.com>
2020-12-10 23:01:27 -0700

    Menu System

---
+++ Walk_Run/Walk_Run/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject menu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    private void Pause()
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    public void Load()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
