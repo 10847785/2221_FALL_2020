b4b9f90de065dded290e7e28666ab0b527a52ddd
Ericio <eric322000@gmail.com>
2020-12-12 23:36:34 -0700

    End Game

---
+++ Walk_Run/Walk_Run/Assets/Scripts/AddPoints.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPoints : MonoBehaviour
+{
+    public int addPoints;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WinGame.AddPoints (addPoints);
+    }
+}
---
+++ Walk_Run/Walk_Run/Assets/Scripts/WinGame.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    public static int Score;
+    	public int WinScore;
+    
+    	public Text WinText;
+    
+    	Text ScoreText;
+    
+    	void Awake(){
+    		Time.timeScale = 1;
+    	}
+    
+    	// Use this for initialization
+    	void Start () {
+    		ScoreText = GetComponent<Text>();
+    
+    		Score = 0;
+    
+    		WinText.GetComponent<Text>().enabled = false;
+    	}
+    
+    	// Update is called once per frame
+    	void Update () {
+    		if (Score < 0)
+    			Score = 0;
+    		ScoreText.text = " " + Score;
+    
+    		// If the player win display win test
+    		if(Score >= WinScore ){
+    			print("Win Score Reached =" + Score);
+    			WinText.GetComponent<Text>().enabled = true;
+    			Time.timeScale = 0;
+    		}
+    		// If player hits the Escape key return to start menu
+    		if(Input.GetKeyDown(KeyCode.Escape)){
+    			SceneManager.LoadScene(0);
+    		}
+    
+    	}
+    
+    	public static void AddPoints (int PointsToAdd) {
+    		Score += PointsToAdd;
+    	}
+}
